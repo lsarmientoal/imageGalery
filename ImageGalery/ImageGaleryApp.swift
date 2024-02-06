9a0159f0910f7d0ebe49093e2722998aefad246b
Laura Sarmiento <lsarmientoal@gmail.com>
2024-02-06 07:58:39 -0500

    Initial Commit

---
+++ ImageGalery/ImageGaleryApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImageGaleryApp.swift
+//  ImageGalery
+//
+//  Created by Lau Sarmiento on 6/02/24.
+//
+
+import SwiftUI
+
+@main
+struct ImageGaleryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
